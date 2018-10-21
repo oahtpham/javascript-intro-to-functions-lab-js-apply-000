cdb4652c1575f186c268e666dea22f299a1d89cf
Thao Pham <phamthaop.9@gmail.com>
2018-10-21 23:11:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,5 +17,6 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return `I can't hear you!`
+  } else if {string === string.toUpperCase
   }
 }
